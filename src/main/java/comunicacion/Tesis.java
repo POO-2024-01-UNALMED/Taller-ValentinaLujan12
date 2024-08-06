247b15e703048f021fedfde2203113f36a99d7f9
ValentinaLujan12 <159487000+ValentinaLujan12@users.noreply.github.com>
2024-08-05 19:27:01 -0500

    Update Tesis.java

--- src/main/java/comunicacion/Tesis.java
+++ src/main/java/comunicacion/Tesis.java
@@ -7,18 +7,17 @@ public class Tesis extends Escrito{
 	private String conclusion;
 	private String referencias;
 	private String interpretacion;
-
-
+	
     //Constructor
-    public Tesis(String origen, String titulo, String autor, int paginas, String interpretacion, String idea, String[] argumentos, String conclusion, String referencias) {
-        super(origen, titulo, autor, paginas);
-        this.idea = idea;
-        this.argumentos = argumentos;
-        this.conclusion = conclusion;
-        this.referencias = referencias;
-        this.interpretacion = interpretacion;
-    }
-
+	public Tesis(String origen, String titulo, String autor, int paginas, String idea, String[] argumentos, String conclusion, String referencias, String interpretacion) {
+		super(origen, titulo, autor, paginas);
+		this.idea = idea;
+		this.argumentos = argumentos;
+		this.conclusion = conclusion;
+		this.referencias = referencias;
+		this.interpretacion = interpretacion;
+	}
+	
 
     // Metodos
     @Override
@@ -40,42 +39,42 @@ public class Tesis extends Escrito{
 
     //Getters y setters 
     public String getIdea() {
-        return idea;
-    }
-
-    public void setIdea(String idea) {
-        this.idea = idea;
-    }
-
-    public String[] getArgumentos() {
-        return argumentos;
-    }
-
-    public void setArgumentos(String[] argumentos) {
-        this.argumentos = argumentos;
-    }
-
-    public String getConclusiones() {
-        return conclusiones;
-    }
-
-    public void setConclusiones(String conclusiones) {
-        this.conclusiones = conclusiones;
-    }
-
-    public String getReferencias() {
-        return referencias;
-    }
-
-    public void setReferencias(String referencias) {
-        this.referencias = referencias;
-    }
-
-    public String getInterpretacion() {
-        return interpretacion;
-    }
-
-    public void setInterpretacion(String interpretacion) {
-        this.interpretacion = interpretacion;
-    }
+		return idea;
+	}
+	
+	public void setIdea(String idea) {
+		this.idea = idea;
+	}
+	
+	public String[] getArgumentos() {
+		return argumentos;
+	}
+	
+	public void setArgumentos(String[] argumentos) {
+		this.argumentos = argumentos;
+	}
+	
+	public String getConclusion() {
+		return conclusion;
+	}
+	
+	public void setConlusion(String conclusion) {
+		this.conclusion = conclusion;
+	}
+	
+	public String getReferencias() {
+		return referencias;
+	}
+	
+	public void setReferencias(String referencias) {
+		this.referencias = referencias;
+	}
+	
+	public String getInterpretacion() {
+		return interpretacion;
+	}
+	
+	public void setInterpretacion(String interpretacion) {
+		this.interpretacion = interpretacion;
+	}
 }
